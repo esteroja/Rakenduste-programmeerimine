cb869f85a86e7356f7f307f5c1cf75d9b46b13ed
Ester Ojala <146342718+esteroja@users.noreply.github.com>
2024-10-04 01:27:14 +0300

    feat: add express validator

--- 2024-09-23/typhoon/backend/index.js
+++ 2024-09-23/typhoon/backend/index.js
@@ -9,6 +9,8 @@ const todoRoutes = require("./routes/todo.routes");
 
 const jwt = require("jsonwebtoken");
 
+const { body, validationResult } = require("express-validator");
+
 app.use(express.json());
 app.use(cors());
 
@@ -20,43 +22,68 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/token", (req, res) => {
-  const title = req.body.title;
-  const priority = req.body.priority;
-  const id = Date.now();
-  const createdAt = Date.now();
-  const updatedAt = null;
-  const deleted = false;
+app.get(
+  "/token",
+  body("title").isString(),
+  body("priority").isInt(),
+  (req, res) => {
+    const result = validationResult(req);
 
-  const payload = {
-    id,
-    title,
-    priority,
-    createdAt,
-    updatedAt,
-    deleted,
-  };
-
-  const token = jwt.sign(payload, "secret", {
-    expiresIn: "1h",
-  });
-
-  console.log(`Token: ${token}`);
-  res.json({ token });
-});
-
-app.post("/verify", (req, res) => {
-  const token = req.body.token;
-  jwt.verify(token, "secret", (err, decoded) => {
-    if (err) {
-      console.log("Token not valid");
-      return res.json({ valid: false, message: "Token not valid" });
+    if (!result.isEmpty()) {
+      return res.send({ errors: result.array() });
     }
-    console.log("Token valid");
-    console.log(`Title: ${decoded.title}. Priority: ${decoded.priority}`);
-    res.json({ valid: true, decoded });
-  });
-});
+
+    const title = req.body.title;
+    const priority = req.body.priority;
+    const id = Date.now();
+    const createdAt = Date.now();
+    const updatedAt = null;
+    const deleted = false;
+
+    const payload = {
+      id,
+      title,
+      priority,
+      createdAt,
+      updatedAt,
+      deleted,
+    };
+
+    const token = jwt.sign(payload, "secret", {
+      expiresIn: "1h",
+    });
+
+    console.log(`Token: ${token}`);
+    res.json({ token });
+  }
+);
+
+app.post(
+  "/verify",
+  body("token")
+    .isString()
+    .withMessage("Token is not a string")
+    .notEmpty()
+    .withMessage("Token is empty"),
+  (req, res) => {
+    const result = validationResult(req);
+
+    if (!result.isEmpty()) {
+      return res.send({ errors: result.array() });
+    }
+
+    const token = req.body.token;
+    jwt.verify(token, "secret", (err, decoded) => {
+      if (err) {
+        console.log("Token not valid");
+        return res.json({ valid: false, message: "Token not valid" });
+      }
+      console.log("Token valid");
+      console.log(`Title: ${decoded.title}. Priority: ${decoded.priority}`);
+      res.json({ valid: true, decoded });
+    });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
